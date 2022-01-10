0fa1a5e0edef355b0e94e07c4e9ea3943357a7eb
BAKO ADAMOU <barikibako@gmail.com>
2022-01-10 19:38:03 +0100

    Modification de server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
 
 const express = require('express');
+
+const path = require('path');
+
 const app = express();
 
 // const bodyParser = require('body-parser');
@@ -14,12 +17,17 @@ const app = express();
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/ara-livraison'));
+// app.use(requireHTTPS);
+app.use(express.static(__dirname +'/dist/ara-livraison'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/ara-livraison/'});
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+    '/dist/ara-livraison/index.html'));
 });
+
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: 'dist/ara-livraison/'});
+// });
 
 // var corsOptions = {
 //     origin: 'http://localhost:9191',
@@ -28,4 +36,4 @@ app.get('/*', function(req, res) {
 // app.use(cors(corsOptions));
 
 app.listen(process.env.PORT || 8080);
-console.log("App listening on port 8082");
+console.log("App listening on port 8080");
